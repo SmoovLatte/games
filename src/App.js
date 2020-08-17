dd221ee5909d087a00a6f358a8fc3b6a4abf2a2f
Sointu Laine <so1310la-s@lu.se>
2020-08-17 16:18:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// // you can write classes or method components in React
+
+// class App extends React.Component {
+//   render() {
+//     return <div>Hello, world!</div>;
+//   }
+// }
+
+function App() {
+  return <div>Hello, world!</div>;
+}
+
+export default App;
